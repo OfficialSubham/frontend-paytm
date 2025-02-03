fdc4ee40d5da81ff1dbe52da542bc4bfaf95788a
Subham Mondal <168335967+OfficialSubham@users.noreply.github.com>
2025-02-03 14:08:15 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
-import dotenv from "dotenv";
+import { config } from "dotenv";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-dotenv.config()
+config()
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   define: {
-    "process.env": process.env,
+    "process.env": process.env
   },
 });
