553dc3bcf04575d8d60308f2f28a144ce4e24674
Subham Mondal <168335967+OfficialSubham@users.noreply.github.com>
2025-02-03 14:14:59 +0530

    Update vite.config.js deleted

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,10 @@
-import { config } from "dotenv";
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-config()
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  define: {
-    "process.env": process.env
-  },
+  plugins: [react(), tailwindcss()]
 });
